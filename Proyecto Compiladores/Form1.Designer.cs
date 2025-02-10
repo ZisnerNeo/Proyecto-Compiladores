7da991c6dc181bd1acc963ed8792293cd02e52db
Juan Diego Coronado Viggiano <98432016+DiegoCV16@users.noreply.github.com>
2025-02-10 17:41:55 -0600

    prueba
    Primeras pruebas

--- Proyecto Compiladores/Form1.Designer.cs
+++ Proyecto Compiladores/Form1.Designer.cs
@@ -28,13 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(239, 195);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ejecutar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(225, 167);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(498, 22);
+            this.txtInput.TabIndex = 1;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(225, 245);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(498, 22);
+            this.txtOutput.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
     }
 }
 
--- Proyecto Compiladores/Form1.cs
+++ Proyecto Compiladores/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,5 +17,115 @@ namespace Proyecto_Compiladores
         {
             InitializeComponent();
         }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string infixRegex = txtInput.Text;
+            try
+            {
+                string expandedRegex = ExpandCharacterRanges(infixRegex);
+                string postfixRegex = ConvertToPostfix(AddExplicitConcatenation(expandedRegex));
+                txtOutput.Text = postfixRegex;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+
+        private string ExpandCharacterRanges(string regex)
+        {
+            return Regex.Replace(regex, "\\[([a-zA-Z0-9])-([a-zA-Z0-9])\\]", match =>
+            {
+                char start = match.Groups[1].Value[0];
+                char end = match.Groups[2].Value[0];
+                StringBuilder expanded = new StringBuilder();
+                for (char c = start; c <= end; c++)
+                {
+                    expanded.Append(c);
+                    if (c < end) expanded.Append("|");
+                }
+                return "(" + expanded.ToString() + ")";
+            });
+        }
+
+        private string AddExplicitConcatenation(string regex)
+        {
+            StringBuilder output = new StringBuilder();
+            for (int i = 0; i < regex.Length; i++)
+            {
+                char c = regex[i];
+                output.Append(c);
+
+                if (i < regex.Length - 1)
+                {
+                    char next = regex[i + 1];
+
+                    if ((char.IsLetterOrDigit(c) || c == ')' || c == '*' || c == '?' || c == '+') &&
+                        (char.IsLetterOrDigit(next) || next == '(' || next == '['))
+                    {
+                        output.Append("&");
+                    }
+                }
+            }
+            return output.ToString();
+        }
+
+        private string ConvertToPostfix(string infix)
+        {
+            Dictionary<char, int> precedence = new Dictionary<char, int>
+            {
+                {'|', 1},
+                {'&', 2}, // Concatenación explícita
+                {'*', 3},
+                {'?', 3},
+                {'+', 3}
+            };
+
+            Stack<char> operators = new Stack<char>();
+            StringBuilder output = new StringBuilder();
+            Stack<string> operandStack = new Stack<string>();
+
+            for (int i = 0; i < infix.Length; i++)
+            {
+                char c = infix[i];
+
+                if (char.IsLetterOrDigit(c))
+                {
+                    output.Append(c);
+                }
+                else if (c == '(')
+                {
+                    operators.Push(c);
+                }
+                else if (c == ')')
+                {
+                    while (operators.Count > 0 && operators.Peek() != '(')
+                    {
+                        output.Append(operators.Pop());
+                    }
+                    operators.Pop();
+                }
+                else if (precedence.ContainsKey(c))
+                {
+                    while (operators.Count > 0 && precedence.ContainsKey(operators.Peek()) && precedence[operators.Peek()] >= precedence[c])
+                    {
+                        output.Append(operators.Pop());
+                    }
+                    operators.Push(c);
+                }
+            }
+
+            while (operators.Count > 0)
+            {
+                output.Append(operators.Pop());
+            }
+
+            return output.ToString();
+        }
     }
 }
+
